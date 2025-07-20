cfc646ae64c8ac72373f63c3c3f43ef94c82e82a
Daniil <56443917+Danil-Prog@users.noreply.github.com>
2025-07-20 15:33:41 +0700

    Update JwtAuthenticationFilter.java

--- src/main/java/com/gearit/api/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/gearit/api/config/filter/JwtAuthenticationFilter.java
@@ -66,7 +66,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = null;
 
         if (header != null && !header.isBlank() && header.startsWith(BEARER_PREFIX)) {
-            token = header.substring(BEARER_PREFIX.length());
+            token = header.substring(7);
         }
 
         return token;
