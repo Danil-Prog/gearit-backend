8f643340368bedc5eb30c48cb85c7cc5b7cd4dad
Daniil Gavrilov <daniil.gavrilov@eltex.loc>
2025-08-15 10:32:57 +0700

    [backend]: lombok
     - доработал корс

--- src/main/java/com/gearit/api/config/SecurityConfig.java
+++ src/main/java/com/gearit/api/config/SecurityConfig.java
@@ -69,10 +69,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         var configuration = new CorsConfiguration();
+        String allowedUri = corsProperties.getCorsAllowedOriginsUri();
 
-        configuration.setAllowedOriginPatterns(List.of(corsProperties.getCorsAllowedOriginsUri()));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of(allowedUri));
+        configuration.setAllowedMethods(List.of("GET, POST"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
 
         var corsConfigurationSource = new UrlBasedCorsConfigurationSource();
